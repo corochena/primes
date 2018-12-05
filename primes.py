cdc366f0918662a6fbdc70a97c19ca48b13c7865
Carlos Peña <corochena@gmail.com>
2018-12-05 16:32:33 -0600

    Calcula los primos hasta 10 millones en poco mas de 3 minutos

--- primes.py
+++ primes.py
@@ -1,23 +1,44 @@
 def primes_file(upto_n, filename):
     primes = [2]
     outputfile = open(filename, "wt")
-    lines = 0
-    msg = ""
 
-    for num in range(primes[-1], upto_n + 1):
+    for num in range(primes[-1] + 1, upto_n + 1, 1):
         p = True
         for div in primes:
             if num % div == 0:
                 p = False
-                msg = " divisible by " + str(div)
+                break
+            if div > num**0.5:
                 break
         if p:
             primes.append(num)
-            msg = " is prime"
 
-        outputfile.write(str(num) + msg + "\n")
-
+    print(len(primes))
     outputfile.write(str(primes))
     outputfile.close()
 
-primes_file(1000, "primos_1000.txt")
+primes_file(1000000, "primos_1000000.txt")
+
+# def primes_file(upto_n, filename):
+#     primes = [2]
+#     outputfile = open(filename, "wt")
+#     lines = 0
+#     msg = ""
+#
+#     for num in range(primes[-1], upto_n + 1):
+#         p = True
+#         for div in primes:
+#             if num % div == 0:
+#                 p = False
+#                 msg = " divisible by " + str(div)
+#                 break
+#         if p:
+#             primes.append(num)
+#             msg = " is prime"
+#
+#         outputfile.write(str(num) + msg + "\n")
+#
+#     outputfile.write(str(primes))
+#     outputfile.close()
+#
+# primes_file(1000, "primos_1000.txt")
